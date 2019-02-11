de48a3da9611fdeddad0d7a3a2d8cc71e1dcf1ae
chan-na <kimhim7025@gmail.com>
2019-02-12 07:16:03 +0900

    UVa 11136 Hoax or what update

---
+++ UVa Online Judge/11136 Hoax or what/WA_11136.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    multiset<int> urn;
+
+    int numOfDays;
+    while (cin >> numOfDays, numOfDays != 0)
+    {
+        urn.clear();
+
+        int k;
+        int bill;
+        int highestBill;
+        int lowestBill;
+        int promotionFee = 0;
+        for (int i = 0; i < numOfDays; ++i)
+        {
+            cin >> k;
+            while (k--)
+            {
+                cin >> bill;
+                urn.insert(bill);
+            }
+
+            auto it = urn.begin();
+            lowestBill = *it;
+            urn.erase(it);
+
+            it = urn.end();
+            --it;
+            highestBill = *it;
+            urn.erase(it);
+
+            promotionFee += highestBill - lowestBill;
+        }
+
+        cout << promotionFee << '\n';
+    }
+
+    return 0;
+}
